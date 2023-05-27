5a5b2ce289e86d71971738832638ffd1fa4551ab
Flame-eth <theflamepen@gmail.com>
2023-05-27 13:00:08 +0100

    ui optimization

--- src/common/footer/Footer.jsx
+++ src/common/footer/Footer.jsx
@@ -6,11 +6,13 @@ const Footer = () => {
   return (
     <>
       <footer>
-        <div className="container grid2">
-          <div className="box">
+        <div className="container grid2 footer">
+          <div className="fLogoCon">
             <div className="logo width ">
               <Link to="/">
-                <h1 className="logo">Click Cart</h1>
+                <h1 className="logo" style={{ color: "white" }}>
+                  Click Cart
+                </h1>
               </Link>
               {/* <img src={logo} alt='' /> */}
             </div>
@@ -19,7 +21,7 @@ const Footer = () => {
               libero id et, in gravida. Sit diam duis mauris nulla cursus. Erat
               et lectus vel ut sollicitudin elit at amet.
             </p>
-            <div className="icon d_flex">
+            <div className="icon fIcons">
               <div className="img d_flex">
                 <i class="fa-brands fa-google-play"></i>
                 <span>Google Play</span>
@@ -31,35 +33,37 @@ const Footer = () => {
             </div>
           </div>
 
-          <div className="box">
-            <h2>About Us</h2>
-            <ul>
-              <li>Careers</li>
-              <li>Our Stores</li>
-              <li>Our Cares</li>
-              <li>Terms & Conditions</li>
-              <li>Privacy Policy</li>
-            </ul>
-          </div>
-          <div className="box">
-            <h2>Customer Care</h2>
-            <ul>
-              <li>Help Center </li>
-              <li>How to Buy </li>
-              <li>Track Your Order </li>
-              <li>Corporate & Bulk Purchasing </li>
-              <li>Returns & Refunds </li>
-            </ul>
-          </div>
-          <div className="box">
-            <h2>Contact Us</h2>
-            <ul>
-              <li>
-                70 Washington Square South, New York, NY 10012, United States{" "}
-              </li>
-              <li>Email: uilib.help@gmail.com</li>
-              <li>Phone: +1 1123 456 780</li>
-            </ul>
+          <div className="boxCon">
+            <div className="box">
+              <h2>About Us</h2>
+              <ul>
+                <li>Careers</li>
+                <li>Our Stores</li>
+                <li>Our Cares</li>
+                <li>Terms & Conditions</li>
+                <li>Privacy Policy</li>
+              </ul>
+            </div>
+            <div className="box">
+              <h2>Customer Care</h2>
+              <ul>
+                <li>Help Center </li>
+                <li>How to Buy </li>
+                <li>Track Your Order </li>
+                <li>Corporate & Bulk Purchasing </li>
+                <li>Returns & Refunds </li>
+              </ul>
+            </div>
+            <div className="box">
+              <h2>Contact Us</h2>
+              <ul>
+                <li>
+                  70 Washington Square South, New York, NY 10012, United States{" "}
+                </li>
+                <li>Email: support@click-cart.com</li>
+                <li>Phone: +11111 2222 3333 </li>
+              </ul>
+            </div>
           </div>
         </div>
       </footer>
